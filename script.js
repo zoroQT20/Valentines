d8d925411982e1a5b59bbb9dc868de8eb4f36ec4
zoroQT20 <155709657+zoroQT20@users.noreply.github.com>
2024-02-14 12:39:29 +0800

    1st commit

--- script.js
+++ script.js
@@ -1,44 +1,47 @@
 const alternatives = [
   {text:"", images:"images/cat-01.gif"},
-  {text:"Te prometo que será inolvidable", images:"images/cat-02.gif"},
-  {text:"Piénsalo de nuevo", images:"images/cat-03.gif"},
-  {text:"Vamos, atrévete a un sí", images:"images/cat-04.gif"},
-  {text:"Que el miedo no te detenga", images:"images/cat-05.gif"},
-]
-const ohyes = {text:"Sabía que aceptarias", images:"images/cat-yes.gif"}
-const cat = document.querySelector('.cat')
-const text = document.querySelector('.text')
-const buttons = document.querySelectorAll('.button')
-const errorButton = document.querySelector('.button__error')
+  {text:"I promise it will be unforgettable", images:"images/cat-02.gif"},
+  {text:"Think it over again", images:"images/cat-03.gif"},
+  {text:"Come on, dare to say yes", images:"images/cat-04.gif"},
+  {text:"Don't let fear stop you", images:"images/cat-05.gif"},
+];
+const ohyes = {text:"I knew you would accept", images:"images/cat-yes.gif"};
+const cat = document.querySelector('.cat');
+const text = document.querySelector('.text');
+const buttons = document.querySelectorAll('.button');
+const errorButton = document.querySelector('.button__error');
 
 let count = 0;
 
 function updateDisplay(item){
-  cat.src = item.images
-  text.innerHTML = item.text
+  cat.src = item.images;
+  text.innerHTML = item.text;
 }
 
 errorButton.addEventListener('click', ()=>{
-  count = 0;
-  updateDisplay(alternatives[count])
-  buttons.forEach(btn => btn.style.display = 'inline-block')
-  errorButton.style.display = 'none'
-})
+  const confirmRestart = confirm("Virus Activated. Click OK to restart.");
+  if (confirmRestart) {
+    count = 0;
+    updateDisplay(alternatives[count]);
+    buttons.forEach(btn => btn.style.display = 'inline-block');
+    errorButton.style.display = 'none';
+  }
+});
 
 buttons.forEach(button => {
   button.addEventListener('click', ()=>{
-      if(button.textContent == "Si"){
-          updateDisplay(ohyes)
-          buttons.forEach(btn => btn.style.display = 'none')
+      if(button.textContent == "Yes"){
+          updateDisplay(ohyes);
+          buttons.forEach(btn => btn.style.display = 'none');
       }
       if(button.textContent == 'No'){
-          count++
+          count++;
           if(count < alternatives.length){
-              updateDisplay(alternatives[count])
+              updateDisplay(alternatives[count]);
           }else{
-              buttons.forEach(btn => btn.style.display = 'none')
-              errorButton.style.display = 'inline-block'
+              buttons.forEach(btn => btn.style.display = 'none');
+              errorButton.style.display = 'inline-block';
           }
       }
-  })
-})
+  });
+});
